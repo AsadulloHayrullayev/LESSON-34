2daee046f185a8727e4eea871a0cfa8077b9b57c
Asadullo <hayrullayevasadullo@gmail.com>
2024-06-02 19:21:16 +0500

    4th

--- main.py
+++ main.py
@@ -6,11 +6,24 @@
 # print(a)
 
 #2
-a = 46
-b = 'string'
-print(a*2,b*2)
+# a = 46
+# b = 'string'
+# print(a*2,b*2)
+#
+# #3
+# a = (5,'F','Privet',90.2)
+# b = {67}
+# print(a[2])
 
-#3
-a = (5,'F','Privet',90.2)
-b = {67}
-print(a[2])
+#4
+def fourth():
+    user_chislo = input("Gimme 4-znchnoe chislo: ")
+
+    if user_chislo.isdigit() and len(user_chislo) == 4:
+        number = [int(number) for number in user_chislo]
+        print("Cifri vashego chisla: ", number)
+    else:
+        print("eto ne 4-znachnoe chislo!")
+
+fourth()
+
